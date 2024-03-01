c9e60e8e3d9b7e91e4fd19706cd10e53adf8029d
MunJitso <72764494+MunJitso@users.noreply.github.com>
2024-03-01 21:12:44 +0100

    Update source_code.ino

--- source_code.ino
+++ source_code.ino
@@ -17,8 +17,9 @@
 MPU6050 mpu;
 Servo servoMotor;
 
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
+int16_t ax, ay, az, gx, gy, gz;
+
+const int MPU_addr = 0x68;
 
 int16_t ServoPos = 90;
 
@@ -27,7 +28,7 @@ int in1 = 2;
 int in2 = 3;
 int in3 = 4;
 int in4 = 5;
-
+ 
 // Infra red senors' Pins
 int right = 6;
 int left = 7;
@@ -35,6 +36,12 @@ int left = 7;
 // Servo Motor's pin
 int servoPin = 8;
 
+const double R_angle = 0.0067;  // Measurement noise covariance
+const double Q_angle = 0.001; // Process noise covariance
+const double dt = 0.015;      // Time step (15ms in my configuration)
+
+double angle = 90;
+double P[2][2] = {{1, 0}, {0, 1}}; // Initial error covariance matrix
 TaskHandle_t gyroHandler;
 TaskHandle_t mvtHandler;
 
@@ -47,7 +54,7 @@ void setup() {
 
   pinMode(right, INPUT);
   pinMode(left, INPUT);
-  
+
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
@@ -57,26 +64,53 @@ void setup() {
 
   mpu.setFullScaleGyroRange(MPU6050_GYRO_FS_250);
 
+  mpu.CalibrateGyro();
+
   servoMotor.attach(servoPin);
-  servoMotor.write(90);
+  servoMotor.write(angle);
 
   xTaskCreate(gyroTask, "GyroTask", 128, NULL, 1, &gyroHandler);
   xTaskCreate(mvtTask, "MvtTask", 128, NULL, 2, &mvtHandler);
 }
 
-void loop() { // Nothing here because we are using Threads (RTOS)
+void loop() {  // Nothing here because we are using Threads (RTOS)
 }
 
 void gyroTask(void *pvParameters) {
   while (true) {
     mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
-    // Adjust the servo position based on gyroscopic data
-    ax = map(ax, -17000, 17000, 0, 180);  // Adjust based on Gyro_x reading
-    Serial.println(ax);
-    servoMotor.write(ax);
+    double gyroRate = gx / 131.0; 
 
-    delay(250);
+    angle += dt * gyroRate;
+
+    P[0][0] += dt * (dt * P[1][1] - P[0][1] - P[1][0] + Q_angle);
+    P[0][1] -= dt * P[1][1];
+    P[1][0] -= dt * P[1][1];
+    P[1][1] += Q_angle * dt;
+
+    ax = map(ax, -18000, 18000, 0, 180);
+    double y = ax; 
+    double S = P[0][0] + R_angle;
+    double K[2];  // Kalman gain
+    K[0] = P[0][0] / S;
+    K[1] = P[1][0] / S;
+
+    angle += K[0] * (y - angle);
+    P[0][0] -= K[0] * P[0][0];
+    P[0][1] -= K[0] * P[0][1];
+    P[1][0] -= K[1] * P[0][0];
+    P[1][1] -= K[1] * P[0][1];
+
+    // Update servo position
+    servoMotor.write(angle);
+
+
+    // For Debugging
+    // Serial.print("Angle: ");
+    // Serial.println(angle);
+
+    delay(15);
   }
 }
 
